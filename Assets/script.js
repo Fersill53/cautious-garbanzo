4bb4c0bc3728fd6d9e3ab0163e7c7a2c0c1c8801
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-04-14 23:50:15 -0500

    js edits/css/html

--- Assets/script.js
+++ Assets/script.js
@@ -1,93 +1,30 @@
-var quizArea = document.getElementById("quiz");
-var startBtn = document.getElementById("startBtn");
+const quizArea = document.getElementById("quiz");
+const startBtn = document.getElementById("startBtn");
+const nxtBtn = document.getElementById('nxtBtn')
+const questionContainerElement = document.getElementById('questBox');
+let shuffledQuestions, currentQuestionIndex;
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answerBtn');
+
 startBtn.addEventListener('click', startGame);
-var questionContainerElement = document.getElementById('questBox');
-var shfQuestions, currentQuestionIndex;
-var questionElement = document.getElementById('question');
-var answerButtonsElement = document.getElementById('answerBtn');
-
-var timerCount = 60;
-var score;
-
-    var questions = [
-        {
-            question: "What team is the best in the Premier league?",
-            answerChoices: [
-                {text: 'Manchester United', correct: true}, 
-                {text: 'Liverpool', correct: false}, 
-                {text: 'Manchester City', correct: false}, 
-                {text: 'Arsenal', correct: false}
-            ]
-            
-        },
-
-        {
-            question: "What team is the bandwagon team in the Premier league?",
-            answerChoices: [
-                {text: 'Manchester United', correct: false}, 
-                {text: 'Liverpool', correct: false}, 
-                {text: 'Manchester City', correct: true}, 
-                {text: 'Arsenal', correct: false}
-            ]
-
-        },
-
-        {
-            question: "What team is the best in La Liga?",
-            answerChoices: [
-                {text: "Barcelona", correct: false}, 
-                {text: "Villareal", correct: false}, 
-                {text: "Real Madrid", correct: true}, 
-                {text: "Valencia", correct: false}]
-            
-        },
-
-        {
-            question: "Who is the best soccer player in the World?",
-            answerChoices: [
-                {text:"Mbappe", correct: false}, 
-                {text: "Pele", correct: false}, 
-                {text: "Messi", correct: false}, 
-                {text: "Ronaldo", correct: true}],
-            
-        },
-
-        {
-            question: "What team is underrated in the Premier league?",
-            answerChoices: [
-                {text: "Wolves", correct: false}, 
-                {text: "Brighton", correct: false}, 
-                {text: "Westham", correct: false}, 
-                {text: "New Castle", correct: true}]
-            
-        },
-
-        {
-            question: "What is the word the knights who say ni cannot hear?",
-            answerChoices: ["that", "is", "herring", "it"],
-            
-        },
-
-    ]
-
-
 
 function startGame() {
     console.log('lets go')
     startBtn.classList.add('hide')
-    shfQuestions = questions.sort(() => Math.random() - .5)
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
-    showQuestion(shfQuestions[currentQuestionIndex])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answers.forEach(answerChoices => {
+    question.answerChoices.forEach(answerChoices => {
     const button = document.createElement('button')
     button.innerText = answerChoices.text
     button.classList.add('btn')  
@@ -95,10 +32,102 @@ function showQuestion(question) {
         button.dataset.correct = answerChoices.correct
     } 
     button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild()
+    answerButtonsElement.appendChild(button)
     })
 }
 
-function selectAnswer() {
-
+function resetState() {
+    nxtBtn.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
+
+function selectAnswer(e) { 
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: "What team is the best in the Premier league?",
+        answerChoices: [
+            {text: 'Manchester United', correct: true}, 
+            {text: 'Liverpool', correct: false}, 
+            {text: 'Manchester City', correct: false}, 
+            {text: 'Arsenal', correct: false}
+        ]
+        
+    },
+
+    {
+        question: "What team is the bandwagon team in the Premier league?",
+        answerChoices: [
+            {text: 'Manchester United', correct: false}, 
+            {text: 'Liverpool', correct: false}, 
+            {text: 'Manchester City', correct: true}, 
+            {text: 'Arsenal', correct: false}
+        ]
+
+    },
+
+    {
+        question: "What team is the best in La Liga?",
+        answerChoices: [
+            {text: "Barcelona", correct: false}, 
+            {text: "Villareal", correct: false}, 
+            {text: "Real Madrid", correct: true}, 
+            {text: "Valencia", correct: false}]
+        
+    },
+
+    {
+        question: "Who is the best soccer player in the World?",
+        answerChoices: [
+            {text:"Mbappe", correct: false}, 
+            {text: "Pele", correct: false}, 
+            {text: "Messi", correct: false}, 
+            {text: "Ronaldo", correct: true}],
+        
+    },
+
+    {
+        question: "What team is underrated in the Premier league?",
+        answerChoices: [
+            {text: "Wolves", correct: false}, 
+            {text: "Brighton", correct: false}, 
+            {text: "Westham", correct: false}, 
+            {text: "New Castle", correct: true}]
+        
+    },
+
+    {
+        question: "What is the word the knights who say ni cannot hear?",
+        answerChoices: [
+            {text: 'that', correct: false}, 
+            {text: "it", correct: true}, 
+            {text: "is", correct: false}, 
+            {text: "herring", correct: false}
+        ]
+        
+    }
+
+]
